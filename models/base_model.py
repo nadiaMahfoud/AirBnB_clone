aa3a08cd9a2d2eaa24de9b7cbaac629fa88f09ad
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-09-16 18:32:49 -0700

    syntax error fixed

--- models/base_model.py
+++ models/base_model.py
@@ -25,11 +25,11 @@ class BaseModel:
                             kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[arg_name] = kwargs[arg_name]
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
-                    storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """This method returns an official string representation"""
